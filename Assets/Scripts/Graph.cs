b4df1a853bda04fa00a95237203597aab6beef47
dubu-0 <yudin01@mail.ru>
2022-01-31 21:13:00 +0300

    Graph created

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace BuildingAGraph
+{
+	public class Graph : MonoBehaviour
+	{
+		[SerializeField] private Transform _pointPrefab;
+		[SerializeField, Range(1, 10000)] private int _pointNumber = 1;
+		[SerializeField, Range(1f, 3000f)] private float _graphScale = 1f;
+
+		private bool _rebuildIsNeeded;
+		private readonly List<Transform> _points = new List<Transform>();
+		private readonly FunctionCollection _functionCollection = new FunctionCollection();
+
+		private Func<float, float> _function;
+
+		private void OnValidate()
+		{
+			_rebuildIsNeeded = true;
+		}
+
+		private void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				_function = _functionCollection.GetNext();
+				_rebuildIsNeeded = true;
+			}
+
+			if (_rebuildIsNeeded) 
+				BuildGraph();
+		}
+
+		private void BuildGraph()
+		{
+			if (_points.Count > 0) 
+				DestroyPoints();
+
+			for (var i = 0; i < _pointNumber; i++)
+			{
+				var point = (Transform) PrefabUtility.InstantiatePrefab(_pointPrefab, transform);
+				var step = _graphScale * 2 / _pointNumber;
+				point.localScale = Vector3.one * step;
+
+				var x = (i + 0.5f) * step - _graphScale;
+				var y = _function?.Invoke(x) ?? x;
+
+				if (y is float.NaN)
+					continue;
+
+				point.position = new Vector3(x, y);
+				_points.Add(point);
+			}
+
+			_rebuildIsNeeded = false;
+		}
+
+		private void DestroyPoints()
+		{
+			foreach (var point in _points)
+				Destroy(point.gameObject);
+
+			_points.Clear();
+		}
+	}
+}
